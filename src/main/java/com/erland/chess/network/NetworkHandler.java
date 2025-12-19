7351c31dc5b5232ca3360a94bd649529646d5a4a
erlaaaand <erlandagsya2@gmail.com>
2025-12-19 20:02:09 +0700

    refactor network handler

--- src/main/java/com/erland/chess/network/NetworkHandler.java
+++ src/main/java/com/erland/chess/network/NetworkHandler.java
@@ -1,12 +1,12 @@
 package com.erland.chess.network;
 
 import com.erland.chess.model.Board;
-import com.erland.chess.view.BoardPanel;
+import com.erland.chess.view.BoardView;
 
 public interface NetworkHandler {
     void sendMove(Board.Move move);
     void sendSurrender();
     void sendCancel();
-    void setBoardPanel(BoardPanel panel);
+    void setBoardPanel(BoardView panel);
     void close();
 }
